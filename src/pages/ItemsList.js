64fdd402f49f4952ec746d3031d765566a680bdc
Luanna Rodrigues <luanna.rodrigues@hotmail.com>
2022-04-06 17:05:09 -0300

    correcao

--- src/pages/ItemsList.js
+++ src/pages/ItemsList.js
@@ -1,7 +1,8 @@
 const itemsList = [
   {
     id: 2395,
-    original: "https://my4kwallpapers.com/wp-content/uploads/2021/05/4k-Aesthetic-Wallpaper.jpg",
+    original:
+      "https://my4kwallpapers.com/wp-content/uploads/2021/05/4k-Aesthetic-Wallpaper.jpg",
   },
   {
     id: 2397,
@@ -9,15 +10,18 @@ const itemsList = [
   },
   {
     id: 2697,
-    original: "https://coolhdwall.com/storage/202101/miles-morales-spider-man-into-the-spider-verse-hd-wallpaper-1920x1080-24.jpg",
+    original:
+      "https://coolhdwall.com/storage/202101/miles-morales-spider-man-into-the-spider-verse-hd-wallpaper-1920x1080-24.jpg",
   },
   {
     id: 3697,
-    original: "https://my4kwallpapers.com/wp-content/uploads/2021/05/4k-Aesthetic-Wallpaper-For-Desktop.jpg",
+    original:
+      "https://my4kwallpapers.com/wp-content/uploads/2021/05/4k-Aesthetic-Wallpaper-For-Desktop.jpg",
   },
   {
     id: 4397,
-    original: "https://my4kwallpapers.com/wp-content/uploads/2021/05/Aesthetic-4k-Wallpapers.jpg",
+    original:
+      "https://my4kwallpapers.com/wp-content/uploads/2021/05/Aesthetic-4k-Wallpapers.jpg",
   },
 ];
 
