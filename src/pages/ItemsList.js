2923ca515f8bf005f05310e9477e16bab6cfd84b
Luanna Rodrigues <luanna.rodrigues@hotmail.com>
2022-04-06 17:04:15 -0300

    slide

--- src/pages/ItemsList.js
+++ src/pages/ItemsList.js
@@ -1,15 +1,23 @@
 const itemsList = [
   {
     id: 2395,
-    original: "https://cdn.wallpapersafari.com/44/98/WEq3n5.jpg",
+    original: "https://my4kwallpapers.com/wp-content/uploads/2021/05/4k-Aesthetic-Wallpaper.jpg",
   },
   {
     id: 2397,
-    original: "https://cdn.wallpapersafari.com/83/86/0sLSPV.jpg",
+    original: "https://cdn.wallpapersafari.com/93/37/jNp5ia.jpg",
   },
   {
     id: 2697,
-    original: "https://wallpaperaccess.com/full/2785492.jpg",
+    original: "https://coolhdwall.com/storage/202101/miles-morales-spider-man-into-the-spider-verse-hd-wallpaper-1920x1080-24.jpg",
+  },
+  {
+    id: 3697,
+    original: "https://my4kwallpapers.com/wp-content/uploads/2021/05/4k-Aesthetic-Wallpaper-For-Desktop.jpg",
+  },
+  {
+    id: 4397,
+    original: "https://my4kwallpapers.com/wp-content/uploads/2021/05/Aesthetic-4k-Wallpapers.jpg",
   },
 ];
 
